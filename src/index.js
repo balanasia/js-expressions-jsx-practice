c1072f844748a365e6cc739505fca1e76d016620
Anastasiia <41988936+balanasia@users.noreply.github.com>
2022-07-14 15:19:44 -0500

    finished the challenge

--- src/index.js
+++ src/index.js
@@ -6,3 +6,16 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Anastasiia";
+const currentYear = new Date().getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {currentYear}</p>
+  </div>,
+  document.getElementById("root")
+);
